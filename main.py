88fbb6ff7d2c41c3a06f08eeca0cd3d2c79c1ec5
Miłosz Gajewski <mgmilo100@gmail.com>
2024-03-03 18:25:54 +0100

    AGPD1 - Pipeline structure.

--- main.py
+++ main.py
@@ -2,268 +2,111 @@ import argparse
 import json
 import os
 import random
+import sys
 
 import cv2
 
-from utilities.image_processing import (
-    alpha_blend,
-    get_contours,
-    get_dilated_mask,
-    get_mask_contours,
-    get_mask_from_contours,
-    odd,
-    seamless_clone,
-    smooth_mask,
-)
 from utilities.parsing_vaildator import dir_path, file_path
-
-
-def empty_callback(value):
-    pass
-
-
-# Ensure and set kernel trackbars values to be odd values
-def odd_dilation_callback(value):
-    value = odd(value)
-    cv2.setTrackbarPos("Dilation length", "Output image preview", value)
-
-
-def odd_gaussian_blur_callback(value):
-    # Ensure the minimum value is 1
-    if value < 1:
-        value = 1
-    value = odd(value)
-    cv2.setTrackbarPos("Gaussian blur kernel (smooth mask)", "Output image preview", value)
-
-
-def odd_blurred_mask_callback(value):
-    if value < 1:
-        value = 1
-    value = odd(value)
-    cv2.setTrackbarPos("Blur length", "Output image preview", value)
+from utilities.pipeline_steps import (
+    add_category_to_coco,
+    add_image_to_coco,
+    create_empty_input_coco_file,
+    get_background_paths,
+    get_random_background,
+    get_random_image_name_for_object_extraction,
+    process_background_image,
+    save_output_coco_to_file,
+)
 
 
 def main(
     coco_filepath: file_path,
     image_library_path: dir_path,
-    input_image_name: str,
-    output_image_filepath: file_path,
-    x_position: int,
-    y_position: int,
-    scale: float,
-    object_index: int,
-    method: str,
+    backgrounds_directory_path: dir_path,
+    output_direcotry_path: dir_path,
+    output_photo_number: int,
 ):
     # Constants
-    POSITION_INCREMENT_DECREMENT = 10
-    SCALE_INCREMENT_DECREMENT = 0.1
-    ALPHA_BLEND_OVERLAY = "alpha_blend"
-    SEAMLESS_CLONE_OVERLAY = "seamless_clone"
+    MIN_NUMBER_OF_OBJECTS_ON_OUTPUT_BACKGROUND = 1
+    MAX_NUMBER_OF_OBJECTS_ON_OUTPUT_BACKGROUND = 15
+    OUTPUT_COCO_FILE_NAME = "annotations_auto_pipeline.json"
+    GENERATED_PHOTO_NAME_PREFIX = "agp_photo_"
+    GENERATED_PHOTO_NAME_SUFFIX = ".jpg"
 
-    # Print input data
-    print("--- Synthetic data generator ---")
-    print(f"Input image: {os.path.join(image_library_path, input_image_name)}")
-    print(f"Output image: {output_image_filepath}")
+    print("--- Auto pipeline synthetic data generator ---")
 
-    # Get data
-    image_path = os.path.join(image_library_path, input_image_name)
-    image = cv2.imread(image_path)
-    cv2.namedWindow("Output image preview")
-    # Create trackbars to set parameters
-    # Get dilated mask function trackbar
-    cv2.createTrackbar("Dilation length", "Output image preview", 51, 255, odd_dilation_callback)
-    # Smooth mask function trackbars
-    cv2.createTrackbar(
-        "Gaussian blur kernel (smooth mask)", "Output image preview", 75, 555, odd_gaussian_blur_callback
-    )
-    cv2.createTrackbar("Threshold value", "Output image preview", 128, 255, empty_callback)
-    cv2.createTrackbar("Max value with thresh binary", "Output image preview", 255, 255, empty_callback)
-    # Blurred mask
-    cv2.createTrackbar("Blur length", "Output image preview", 149, 255, odd_blurred_mask_callback)
-    annotations = json.load(open(coco_filepath))
+    # Read data from input COCO file
+    with open(coco_filepath, "r") as file:
+        input_coco_file = json.load(file)
+
+    # Prepare output COCO file
+    output_coco_file = create_empty_input_coco_file()
+    rubbish_category_id = add_category_to_coco(output_coco_file, "", "rubbish")
+
+    # Check how many background images contains input directory
+    background_paths = get_background_paths(backgrounds_directory_path)
+    if len(background_paths) == 0:
+        print("No background photos were found in input directory.")
+        sys.exit()
 
     # Main loop
-    while True:
-        # Process mask
-        contours, number_of_objects_in_the_photo = get_contours(annotations, input_image_name, object_index)
-        mask = get_mask_from_contours(image, contours)
+    for photo_num in range(output_photo_number):
+        # Get random background
+        background = get_random_background(background_paths)
+        background = process_background_image(background)
+        height, width, _ = background.shape
 
-        if mask is not None:
-            # Get dilated mask. Bigger dilation_length - more expanded boundaries around litter image
-            dilation_length = cv2.getTrackbarPos("Dilation length", "Output image preview")
-            dilated = get_dilated_mask(mask, dilation_length)
-            # Get smoothed mask. Smaller values of gaussian_blur_kernel will preserve more details, while larger
-            # values will result in more smoothing
-            gaussian_blur_kernel = cv2.getTrackbarPos("Gaussian blur kernel (smooth mask)", "Output image preview")
-            # Pixels with intensity values below threshold_value will be set to 0
-            threshold_value = cv2.getTrackbarPos("Threshold value", "Output image preview")
-            # For binary thresholding, pixels above the threshold get set to maxvalue
-            maxvalue = cv2.getTrackbarPos("Max value with thresh binary", "Output image preview")
-            mask_smooth = smooth_mask(dilated, gaussian_blur_kernel, threshold_value, maxvalue)
-            # The size of the kernel for the Gaussian blur applied to smoothed mask
-            blur_length = cv2.getTrackbarPos("Blur length", "Output image preview")
-            mask_blurred = cv2.GaussianBlur(mask_smooth, (blur_length, blur_length), 0)
-            mask_blurred = cv2.cvtColor(mask_blurred, cv2.COLOR_GRAY2BGR)
+        # Add image record to COCO file
+        output_photo_name = GENERATED_PHOTO_NAME_PREFIX + str(photo_num) + GENERATED_PHOTO_NAME_SUFFIX
+        coco_photo_id = add_image_to_coco(output_coco_file, width, height, output_photo_name)
 
-            mask_contour = get_mask_contours(mask_blurred)
-            x, y, w, h = cv2.boundingRect(mask_contour)
+        # Get random number of objects to add to background
+        num_of_objects = random.randint(
+            MIN_NUMBER_OF_OBJECTS_ON_OUTPUT_BACKGROUND, MAX_NUMBER_OF_OBJECTS_ON_OUTPUT_BACKGROUND
+        )
+        for _ in range(num_of_objects):
+            # Get random image from which object will be extracted
+            object_img_detail = get_random_image_name_for_object_extraction(input_coco_file)
+            object_img_path = os.path.join(image_library_path, object_img_detail["file_name"])
+            object_img_id = object_img_detail["id"]
 
-            img_cropped = image[y : y + h, x : x + w]
-            mask_cropped = mask_blurred[y : y + h, x : x + w]
+            # 2. Weź obiekty ze zdjęcia,
+            # 3. Losuj obiekt ze zdjęcia,
+            # 4. Zapisz dane o obiekcie ze starego zdjęcia - stosunek wysokości i szerokości obiektu do wysokości i szerokości zdjęcia
+            # 5. Opcja: Processing obiektu,
+            # 6. Wklej obiekt na tło,
+            # 7. Przygotuj dane do pliku COCO,
+            # 8. Zapisz annotation do pliku COCO,
 
-            if scale is not None:
-                if not img_cropped.size == 0:
-                    img_cropped = cv2.resize(img_cropped, (0, 0), fx=scale, fy=scale)
-                    mask_cropped = cv2.resize(mask_cropped, (0, 0), fx=scale, fy=scale)
-                    h, w, _ = img_cropped.shape
+        # Save output photo
+        output_photo_path = os.path.join(output_direcotry_path, output_photo_name)
+        # cv2.imwrite(output_photo_path, img)  # TODO: replace img with proper image name
 
-            # Process output image
-            output_image = cv2.imread(output_image_filepath)
-            bg_h, bg_w, _ = output_image.shape
-
-            if x_position is None:
-                x_offset = random.randint(0, bg_w - w)
-                x_position = x_offset
-            else:
-                x_offset = x_position
-                if x_position < 0:
-                    x_offset = 0
-                elif x_position > bg_w - w:
-                    x_offset = bg_w - w - 1
-
-            if y_position is None:
-                y_offset = random.randint(0, bg_h - h)
-                y_position = y_offset
-            else:
-                y_offset = y_position
-                if y_position < 0:
-                    y_offset = 0
-                elif y_position > bg_h - h:
-                    y_offset = bg_h - h - 1
-
-            output_image_copy = output_image.copy()
-
-            if method == SEAMLESS_CLONE_OVERLAY:
-                output_image_copy[y_offset : y_offset + h, x_offset : x_offset + w] = seamless_clone(
-                    output_image_copy[y_offset : y_offset + h, x_offset : x_offset + w], img_cropped, mask_cropped
-                )
-            elif method == ALPHA_BLEND_OVERLAY:
-                output_image_copy[y_offset : y_offset + h, x_offset : x_offset + w] = alpha_blend(
-                    output_image_copy[y_offset : y_offset + h, x_offset : x_offset + w], img_cropped, mask_cropped
-                )
-            else:
-                print("Invalid method chosen. Please choose between 'seamless_clone' and 'alpha_blend'.")
-
-            output = output_image_copy
-
-            # Show result
-            resized_img_to_show = cv2.resize(output, (1280, 720))
-            # resized_img_to_show = cv2.resize(mask_cropped, (1280, 720))
-            cv2.imshow("Output image preview", resized_img_to_show)
-
-            # Key control
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("w"):
-                # Move object up
-                y_position -= POSITION_INCREMENT_DECREMENT
-            elif key == ord("s"):
-                # Move object down
-                y_position += POSITION_INCREMENT_DECREMENT
-            elif key == ord("a"):
-                # Move object left
-                x_position -= POSITION_INCREMENT_DECREMENT
-            elif key == ord("d"):
-                # Move object right
-                x_position += POSITION_INCREMENT_DECREMENT
-            elif key == ord("z"):
-                # Change object
-                object_index += 1
-                if object_index > number_of_objects_in_the_photo - 1:
-                    object_index = 0
-                print(
-                    f"Number of object in the picture: {number_of_objects_in_the_photo}. "
-                    f"Selected item index: {object_index}"
-                )
-            elif key == ord("x"):
-                # Scale object up
-                if scale is None:
-                    scale = 1.0
-                else:
-                    scale += SCALE_INCREMENT_DECREMENT
-            elif key == ord("c"):
-                # Scale object down
-                if scale is None:
-                    scale = 1.0  # Standard value
-                else:
-                    scale -= SCALE_INCREMENT_DECREMENT
-                if scale < 0.1:
-                    scale = SCALE_INCREMENT_DECREMENT
-                    print("Minimum scale reached.")
-            elif key == ord("e"):
-                # Save photo
-                cv2.imwrite(output_image_filepath, output)
-                print("Photo saved.")
-            elif key == ord("q"):
-                # Quit
-                break
-
-            # Element position protection
-            if 0 > y_position:
-                y_position = 0
-            elif y_position > bg_h - h:
-                y_position = bg_h - h
-
-            if 0 > x_position:
-                x_position = 0
-            elif x_position > bg_w - w:
-                x_position = bg_w - w
-
-            # Element scale size protection
-            if scale is not None:
-                if not img_cropped.size == 0:
-                    mask_scale_check = cv2.resize(mask_cropped, (0, 0), fx=scale, fy=scale)
-                    h_check, w_check, _ = mask_scale_check.shape
-                    while (h_check > bg_h) or (w_check > bg_w):
-                        scale -= 0.1
-                        mask_scale_check = cv2.resize(mask_cropped, (0, 0), fx=scale, fy=scale)
-                        h_check, w_check, _ = mask_scale_check.shape
-                        print("Maximum scale reached.")
+    print(output_coco_file)
+    # Save output COCO file
+    save_output_coco_to_file(output_direcotry_path, OUTPUT_COCO_FILE_NAME, output_coco_file)
 
     print("--- -------------------- ---")
 
 
 if __name__ == "__main__":
+    DEFAULT_OUTPUT_PHOTO_NUMBER = 100
+
     parser = argparse.ArgumentParser(description="Synthetic data generator")
 
-    parser.add_argument("--coco", type=file_path, help="Pass the COCO file.", required=True)
-    parser.add_argument("--library", type=dir_path, help="Photo library connected to COCO file.", required=True)
+    parser.add_argument("--coco", type=file_path, help="COCO file that describes objects.", required=True)
+    parser.add_argument("--library", type=dir_path, help="Photo library connected to the COCO file.", required=True)
+    parser.add_argument("--input", type=dir_path, help="Backgrounds directory for output photos.", required=True)
+    parser.add_argument("--output", type=dir_path, help="Output directory to store created files.", required=True)
     parser.add_argument(
-        "--input", type=str, help="The name of the photo from which data will be acquired.", required=True
+        "--number", type=int, help="Number of output files.", required=False, default=DEFAULT_OUTPUT_PHOTO_NUMBER
     )
-    parser.add_argument(
-        "--output", type=file_path, help="Path to photo to which the data will be pasted.", required=True
-    )
-    parser.add_argument("--position_x", type=int, help="X position of paste.", default=None)
-    parser.add_argument("--position_y", type=int, help="Y position of paste.", default=None)
-    parser.add_argument("--scale", type=float, help="Scale of paste.", default=None)
-    parser.add_argument("--object", type=int, help="Object index on photo.", default=0)
-    parser.add_argument(
-        "--method",
-        type=str,
-        choices=["seamless_clone", "alpha_blend"],
-        help="Choose the method for blending seamless_clone/alpha_blend.",
-        default="seamless_clone",
-    )
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     main(
         coco_filepath=args.coco,
         image_library_path=args.library,
-        input_image_name=args.input,
-        output_image_filepath=args.output,
-        x_position=args.position_x,
-        y_position=args.position_y,
-        scale=args.scale,
-        object_index=args.object,
-        method=args.method,
+        backgrounds_directory_path=args.input,
+        output_direcotry_path=args.output,
+        output_photo_number=args.number,
     )
---
+++ utilities/pipeline_steps.py
@@ -0,0 +1,193 @@
+import json
+import os
+import cv2
+import random
+import datetime
+
+import numpy
+
+from utilities.parsing_vaildator import dir_path
+
+
+def create_empty_input_coco_file() -> dict:
+    """
+    Creates empty coco file.
+    Returns:
+    Dictionary in COCO format.
+    """
+    output_coco_file = {
+        "images": [],
+        "categories": [],
+        "annotations": [],
+        "licenses": [],
+        "info": {
+            "year": datetime.datetime.now().year,
+            "version": "v1",
+            "description": "Auto generated data from pipeline.",
+            "contributor": "None",
+            "url": "https://github.com/AgniechaP/synthetic_data_generation",
+            "date_created": str(datetime.datetime.now())
+        }
+    }
+    return output_coco_file
+
+
+def add_category_to_coco(coco_file: dict, supercategory: str, name: str) -> int:
+    """
+    Add category to "category" section in COCO.
+    Args:
+        coco_file: Coco file to which data will be added.
+        supercategory: Supercategory name.
+        name: Category name.
+    Returns:
+    Last id of category added.
+    """
+    if len(coco_file["categories"]) != 0:
+        last_category_id = coco_file["categories"][-1]["id"]
+    else:
+        last_category_id = -1
+
+    current_category_id = last_category_id + 1
+    category_body = {
+        "supercategory": supercategory,
+        "id": current_category_id,
+        "name": name
+    }
+
+    coco_file["categories"].append(category_body)
+    return current_category_id
+
+
+def add_image_to_coco(coco_file: dict, width: int, height: int, file_name: str) -> int:
+    """
+    Add image record to "images" list in COCO file.
+    Args:
+        coco_file: Coco file to which data will be added.
+        width: Photo width.
+        height: Photo height.
+        file_name: Name of the photo.
+    Returns:
+    Last id of photo added.
+    """
+    if len(coco_file["images"]) != 0:
+        last_image_id = coco_file["images"][-1]["id"]
+    else:
+        last_image_id = -1
+
+    current_image_id = last_image_id + 1
+    image_body = {
+        "id": current_image_id,
+        "width": width,
+        "height": height,
+        "file_name": file_name,
+        "license": None,
+        "flickr_url": None,
+        "coco_url": None,
+        "date_captured": None,
+        "flickr_640_url": None
+    }
+    coco_file["images"].append(image_body)
+
+    return current_image_id
+
+
+def add_annotation_to_coco(coco_file: dict, image_id: int, category_id: int, segmentation: list[list], area: float, bbox: list, iscrowd: int = 0) -> int:
+    """
+    Add annotation record to "annotations" list in COCO file.
+    Args:
+        coco_file: Coco file to which data will be added.
+        image_id: Image id.
+        category_id: Category of annotation.
+        segmentation: List of lists of segmentation data.
+        area: Area of segmentation.
+        bbox: BoundingBox.
+        iscrowd: Is annotation crowd.
+    Returns:
+    Last id of annotation added.
+    """
+    if len(coco_file["annotations"]) != 0:
+        last_annotation_id = coco_file["annotations"][-1]["id"]
+    else:
+        last_annotation_id = -1
+
+    current_annotation_id = last_annotation_id + 1
+    annotation_body = {
+        "id": current_annotation_id,
+        "image_id": image_id,
+        "category_id": category_id,
+        "segmentation": segmentation,
+        "area": area,
+        "bbox": bbox,
+        "iscrowd": iscrowd
+    }
+    coco_file["annotations"].append(annotation_body)
+
+    return current_annotation_id
+
+
+def get_background_paths(backgrounds_directory_path: dir_path) -> list:
+    """
+    Process and validate path to directory containing background images and get list of background paths.
+    Args:
+        backgrounds_directory_path: path to directory containing background images.
+    Returns:
+        List of background paths.
+    """
+    background_paths = []
+
+    files_in_directory = os.listdir(backgrounds_directory_path)
+    for file in files_in_directory:
+        background_path = os.path.join(backgrounds_directory_path, file)
+        try:
+            photo = cv2.imread(background_path)
+        except:
+            continue
+        if photo is not None:
+            background_paths.append(background_path)
+
+    return background_paths
+
+
+def get_random_background(background_paths: list) -> numpy.ndarray:
+    """
+    Get random background from input paths.
+    Args:
+        background_paths: List of paths to background photos.
+    Returns:
+        Image in RGB OpenCV format.
+    """
+    background_index = random.randint(0, len(background_paths)-1)
+    background_path = background_paths[background_index]
+    background = cv2.cvtColor(cv2.imread(background_path), cv2.COLOR_BGR2RGB)
+
+    return background
+
+
+def process_background_image(background: numpy.ndarray):
+    """
+    Processing background image.
+    Args:
+        background: Background image in RGB OpenCV format.
+    Returns: Processed image in RGB OpenCV format.
+    """
+    # Optional: implement needed processing.
+    return background
+
+
+def get_random_image_name_for_object_extraction(coco_dict: dict) -> dict:
+    number_of_available_images = len(coco_dict["images"])
+    image_index = random.randint(0, number_of_available_images-1)
+    return coco_dict["images"][image_index]
+
+
+def save_output_coco_to_file(output_directory: dir_path, file_name: str, coco_dictionary: dict):
+    """
+    Saves input dictionary to JSON in given file.
+    Args:
+        output_directory: Path to output dictionary.
+        file_name: Name of output COCO file.
+        coco_dictionary: Dictionary with COCO data.
+    """
+    file_path = os.path.join(output_directory, file_name)
+    with open(file_path, "w") as file:
+        json.dump(coco_dictionary, file)
